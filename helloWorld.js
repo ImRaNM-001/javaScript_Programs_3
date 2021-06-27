edec437d22f08f6aeb4d041c3c4ab8d25f5a3925
ImRaNM-001 <imu163@gmail.com>
2021-06-28 02:54:44 +0530

    test-push

--- helloWorld.js
+++ helloWorld.js
@@ -2,4 +2,4 @@ console.log('Hello World');
 
 /*  alert('hello');				// i need to comment this line before running from terminal (cmd) otherwise script won't run.                   
 
-document.write('hey');		// not used widely as it modifies UI by adding additional content to DOM        */
+document.write('hey');		// not used widely as it modifies UI by adding additional content to DOM        */    
